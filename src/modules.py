9b5d368371079317b194ba09f98d8505937c1885
Evgeny <evgeborisov@gmail.com>
2026-01-05 13:23:54 +0300

    Modified mha

--- src/modules.py
+++ src/modules.py
@@ -6,10 +6,11 @@ from pathlib import Path
 
 
 class MultiheadAttention(nn.Module):
-    def __init__(self, d_in, d_out, num_heads, context_length, qkv_bias = False):
+    def __init__(self, d_in, d_out, dropout, num_heads, context_length, qkv_bias = False):
         super().__init__()
 
-        self.d_in = d_in 
+        assert (d_out % num_heads) == 0, 'd_out must be divisible by num_heads' 
+     
         self.d_out = d_out
         self.num_heads = num_heads
         self.head_dim = d_out // num_heads
@@ -17,6 +18,7 @@ class MultiheadAttention(nn.Module):
         self.Key = nn.Linear(d_in, d_out, bias = qkv_bias)
         self.Value = nn.Linear(d_in, d_out, bias = qkv_bias)
         self.out_proj = nn.Linear(d_out, d_out)
+        self.dropout = nn.Dropout(dropout)
         self.register_buffer(
             'mask', torch.triu(torch.ones((context_length, context_length)), diagonal = 1)
         )
@@ -38,11 +40,12 @@ class MultiheadAttention(nn.Module):
         V = V.transpose(1, 2)
         
         att_scores = V @ K.transpose(2, 3)
-        attention_mask = self.mask[ : num_tokens, : num_tokens]
-        att_scores.masked_fill_(attention_mask.bool(), -torch.inf)
+        attention_mask = self.mask.bool()[ : num_tokens, : num_tokens]
+        att_scores.masked_fill_(attention_mask, -torch.inf)
 
         context_vec = att_scores @ V
         context_vec = torch.softmax(context_vec / self.head_dim ** 0.5, dim = -1)
+        context_vec = self.dropout(context_vec).transpose(1, 2)
         context_vec = self.out_proj(context_vec.contiguous().view(batch_size, num_tokens, emb_size))
         
         return context_vec
