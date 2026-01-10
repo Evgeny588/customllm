38481505cb4f5c05c5a589e95b6def9110f50bb2
Evgeny <evgeborisov@gmail.com>
2026-01-10 08:55:15 +0300

    Write model saver

--- train/train.py
+++ train/train.py
@@ -1,6 +1,7 @@
 import torch
 import sys
 import logging
+import time
 import torch.nn.functional as F
 
 from pathlib import Path
@@ -16,9 +17,11 @@ from src.modules import GPTModel, small_config
 
 EPOCHS = 5
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+BATCH_SIZE = 16
 LR = 1e-4
 tokenizer = get_encoding('gpt2')
-
+pin_memory = torch.cuda.is_available()
+num_workers = 2 if pin_memory else 0
 
 class TextDataset(Dataset):
     def __init__(self, text_data, window, tokenizer, slice = 1):
@@ -47,24 +50,39 @@ def main():
         text = file.read()
 
 
-    dataset = TextDataset(text, 6, tokenizer)
+    train_dataset = TextDataset(text, 6, tokenizer)
     train_loader = DataLoader(
-        dataset = dataset,
-        batch_size = 10,
-        shuffle = False
+        dataset = train_dataset,
+        batch_size = BATCH_SIZE,
+        shuffle = False,
+        pin_memory= pin_memory,
+        num_workers= num_workers
     )
-    
+    logging.debug('Train dataset and loader was initialized')
+
+    eval_dataset = TextDataset(text, 6, tokenizer)
+    eval_loader = DataLoader(
+        dataset = eval_dataset,
+        batch_size = BATCH_SIZE,
+        shuffle = False,
+        pin_memory= pin_memory,
+        num_workers= num_workers
+    )
+    logging.debug('Validation dataset and loader was initialized')
+
     model = GPTModel(small_config).to(DEVICE)
     optimizer = torch.optim.AdamW(model.parameters(), lr = LR)
 
-
+    best_eval_loss = 0.0
     train_epoch_loss, eval_epoch_loss = [], []
+    logging.info(f'Start train on {DEVICE}')
+    
     for epoch in tqdm(range(EPOCHS), desc = 'Epoch cycle'):
         
         train_loss = 0.0
         counter_train_batch = 0
         model.train()
-
+        # Forward and backward
         for inputs, targets in tqdm(train_loader, desc = 'Train batch cycle'):
            
             inputs = inputs.to(DEVICE)
@@ -84,11 +102,11 @@ def main():
             counter_train_batch += 1 
             train_loss += loss.item()
             
-        loss_on_epoch = train_loss / counter_train_batch
-        train_epoch_loss.append(loss_on_epoch)
-        logging.info(f'Train loss on {epoch} epoch = {loss_on_epoch: .4f}')
+        train_loss_on_epoch = train_loss / counter_train_batch
+        train_epoch_loss.append(train_loss_on_epoch)
+        logging.info(f'Train loss on {epoch} epoch = {train_loss_on_epoch: .4f}')
 
-
+        # Evaluate
         model.eval()
         eval_loss = 0.0
         counter_eval_batch = 0
@@ -110,9 +128,37 @@ def main():
             counter_eval_batch += 1
             eval_loss += loss.item()
         
-        loss_on_epoch = eval_loss / counter_eval_batch
-        eval_epoch_loss.append(loss_on_epoch)
-        logging.info(f'Validation loss on {epoch} epoch = {loss_on_epoch: .4f}')
+        eval_loss_on_epoch = eval_loss / counter_eval_batch
+        eval_epoch_loss.append(eval_loss_on_epoch)
+        logging.info(f'Validation loss on {epoch} epoch = {eval_loss_on_epoch: .4f}')
+
+        # Saver
+        if eval_loss >= best_eval_loss:
+            best_eval_loss = eval_loss
+
+            torch.save({
+                'metadata': {
+                    'time': time.localtime(),
+                    'epoch': epoch,
+                    'learning_rate': LR,
+                    'batch_size': BATCH_SIZE
+                },
+                'states_dicts': {
+                    'model': model.state_dict(),
+                    'optimizer': optimizer.state_dict()
+                },
+                'losses': {
+                    'train': train_loss_on_epoch,
+                    'val': eval_loss_on_epoch
+                }
+            },
+            root_path / 'checkpoints/best_model.pth')
+
+
+
+
+
+
 
 if __name__ == '__main__':
     main()
